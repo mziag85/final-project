1d7a62f7dd009886eea76ae2f8d28282811466a2
mziag85 <mziag85@gmail.com>
2026-05-08 18:42:06 +0400

    burgen Menu added

---
+++ js.js
@@ -0,0 +1,12 @@
+const burgerMenu = document.querySelector(".burger_menu");
+const navNav = document.querySelector(".nav");
+
+
+burgerMenu.addEventListener("click", function () {
+    
+    
+     navNav.classList.toggle("open");
+     
+     
+        
+});
